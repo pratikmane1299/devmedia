5e808af5301a6b8b3da577b39050c30672b613a3
pratik mane <pratik.mane1299@gmail.com>
2021-06-12 20:44:09 +0530

    :sparkles: Create/Update profile details

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,7 +1,9 @@
 const express = require('express');
-const router = express.Router();
+const { body, validationResult } = require('express-validator');
 
 const Profile = require('../../models/profile');
+
+const router = express.Router();
 
 router.get('/me', async (req, res) => {
   try {
@@ -19,6 +21,70 @@ router.get('/me', async (req, res) => {
     console.error(error.msg);
     return res.status(500).send('Internal server error');
   }
-}); 
+});
+
+router.post('/',
+  body('status', 'Status is required').not().isEmpty(),
+  body('skills', 'Skills is required').not().isEmpty(),
+  async (req, res) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin,
+    } = req.body;
+
+    let profileBody = {};
+    profileBody.user = req.user.id;
+    profileBody.status = status;
+    profileBody.skills = skills.split(',').map(skill => skill.trim());
+    if (company) profileBody.company = company;
+    if (website) profileBody.website = website;
+    if (location) profileBody.location = location;
+    if (bio) profileBody.bio = bio;
+    if (githubusername) profileBody.githubusername = githubusername;
+
+    profileBody.social = {};
+    if (youtube) profileBody.social.youtube = youtube;
+    if (twitter) profileBody.social.twitter = twitter;
+    if (facebook) profileBody.social.facebook = facebook;
+    if (instagram) profileBody.social.instagram = instagram;
+    if (linkedin) profileBody.social.linkedin = linkedin;
+
+    try {
+
+      let profile = await Profile.findOne({ user: req.user.id });
+
+      if (profile) {
+        profile = await Profile.findOneAndUpdate(
+          { user: req.user.id },
+          { $set: profileBody },
+          { new: true }
+        );
+      } else {
+        profile = await Profile.create(profileBody);
+      }
+
+      res.json(profile);
+    } catch(error) {
+      console.error(error);
+      res.status(500).send('Internal server error');
+    }
+  }
+);
 
 module.exports = router;
