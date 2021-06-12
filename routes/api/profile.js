ddff0f0fbb5c0d6b1564c454743fa646f004b4e7
pratik mane <pratik.mane1299@gmail.com>
2021-06-12 17:50:55 +0530

    Create profile model and fetch current user's profile

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const router = express.Router();
 
-const User = require('../../models/user');
+const Profile = require('../../models/profile');
 
 router.get('/me', async (req, res) => {
-  const user = await User.findOne({ _id: req.user.id }, '-password');
-
-  res.json({ user });
-});
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate('user', [
+      'name',
+      'avatar',
+    ]);
+  
+    if (!profile) {
+      return res.status(404).json({ msg: 'Profile for the user not found' });
+    }
+  
+    return res.json(profile);
+  } catch (error) {
+    console.error(error.msg);
+    return res.status(500).send('Internal server error');
+  }
+}); 
 
 module.exports = router;
