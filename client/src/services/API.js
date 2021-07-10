78a9ea187397ac461d9ebf15e95c5d0e7773b5d5
pratik mane <pratik.mane1299@gmail.com>
2021-07-11 00:14:53 +0530

    Remove proxy

--- client/src/services/API.js
+++ client/src/services/API.js
@@ -1,5 +1,9 @@
 import axios from 'axios';
 
+axios.defaults.baseURL =
+  process.env.NODE_ENV === 'production'
+    ? process.env.REACT_APP_API_URL
+    : 'http://localhost:3030';
 axios.interceptors.request.use(function (config) {
 
   const token = localStorage.getItem('token');
